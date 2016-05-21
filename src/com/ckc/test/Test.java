916029933f700df453aa78dd0d097200d15e3ffe
chenkaicheng <ChenKaiCheng520@163.com>
2016-05-21 20:00:26 +0800

    创建了一个测试类

---
+++ src/com/ckc/test/Test.java
@@ -0,0 +1,14 @@
+package com.ckc.test;
+/**  
+* @author:Ckc 
+* @project:Ckc_Test
+* @Title: Test
+* @Package com.ckc.test 
+* @Description: (用一句话描述这个类的作用) 
+* @date 2016年5月21日 下午8:00:03  
+*/
+public class Test
+{
+
+}
+
